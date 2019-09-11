c4a4246818e4e624963e90b5722ca0f4e263904c
Manux <burban.dh@gmail.com>
2019-09-11 17:12:52 -0300

    Se crea formulario Add y Navigation, con sus respectivas configuraciones

--- src/routes/links.js
+++ src/routes/links.js
@@ -3,4 +3,12 @@ const router = express.Router();
 
 const pool = require('../database.js');
 
+router.get('/add', (req, res) => {
+    res.render('links/add');
+});
+
+router.post('/add', (req, res) => {
+    res.send('Recibido...');
+});
+
 module.exports = router;
